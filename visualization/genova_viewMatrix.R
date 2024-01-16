bb0635272ce2a3937d97d7fc0885da990ce48f59
Nuwah12 <ngb23@pitt.edu>
2024-01-16 11:41:15 -0500

    custom color scale

--- visualization/genova_viewMatrix.R
+++ visualization/genova_viewMatrix.R
@@ -4,12 +4,13 @@
 # Noah Burget
 ##########
 library(GENOVA2)
+library(ggplot2)
 
 ## Your objects should be stored in a named list 'explist'
 ## Let's define some parameters for the plots
-CHROM <- '8' # Chromosome name to plot
-START <-  131e6# Start position to plot
-END <-  132e6# End position to plot
+CHROM <- '17' # Chromosome name to plot
+START <-  69e6# Start position to plot
+END <-  71e6# End position to plot
 LOOPS <- NULL # Dataframe represnting a BEDPE file of loop contacts, will be circled on the matrix
 tads <- NULL # Dataframe with TAD boundaries to be plotted
 SCORE.LIMS <- c(0,20) # The range of scores to plot
@@ -19,6 +20,10 @@ plot.legend <- TRUE
 ## Define data to be plotted
 c1 <- explist[[1]]
 c2 <- explist[[2]]
+
+##### Color palette to be used #####
+#COLORS <- c()
+#options("GENOVA.colour.palette" = {COLORS)})
 
 ########## Plotting a single matrix ##########
 ### Here, we can plot 1 or 2 contact matrices and present in different forms
@@ -32,9 +37,13 @@ GENOVA2::hic_matrixplot(exp1 = c1,
                )
 
 # 2: Plot a single PYRAMID-form of a matrix
-GENOVA2::pyramid(exp = mat,
+pdf('figures/MB157_SMC1_WO_HiCmatrix_chr1769000000-71000000.pdf')
+GENOVA2::pyramid(exp = c1,
                  chrom = CHROM, start = START, end = END,
-                 colour = SCORE.LIMS)
+                 colour = SCORE.LIMS)+
+  ggtitle('MB157 WO, chr17:69000000-71000000 @ 10kb')+
+  theme(plot.title = element_text(hjust = 0.5))
+dev.off()
 
 # 3: Plot two matrices about the diagonal (coplot = 'dual'). Each half will be labelled with the objects' name attributes
 GENOVA2::hic_matrixplot(exp1 = c1,
